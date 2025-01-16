5f0cd064e8c43bc10702fbd02bafad0dd13fd540
HadiXq2 <naseemnaseem218031@gmail.com>
2025-01-16 17:53:49 +0500

    index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://hadi-camera.onrender.com";
+var hostURL="https://hadi-camera-jq8x.onrender.com";
 //TOGGLE for Shorters
 var use1pt=false;
 
